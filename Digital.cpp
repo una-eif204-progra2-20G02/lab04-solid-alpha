8df02d13e150735bd5e2d8dece8b660ff65786ee
steveMadrigal <68918625+steveMadrigal@users.noreply.github.com>
2020-08-25 00:34:19 -0600

    Editado v3.00

--- Digital.cpp
+++ Digital.cpp
@@ -17,24 +17,46 @@ void Digital::setDiscount(double discount){
     ICalculusDiscount::_discount = discount;
 }
 void Digital::setTax(double tax){
-    ICalculusTax::tax = tax;
+    ICalculusTax::_tax = tax;
 }
 double Digital::calculatePriceWithTax(){
-    return ICalculateTax::calculatePriceWithTax;
+    if(Digital::getVariable()==true) {
+        return Digital::calculateDiscount()*Digital::getTax();
+    }
+    else if(Digital::getVariable()==false){
+        return Digital::calculateDiscount()*Digital::getTax();
+    }
+}
+
+bool Physical::getVariable() const {
+    return _variable;
+}
+void Physical::setVariable(bool variable) {
+    _varibale=variable;
 }
 double Digital::calculateDiscount(){
-   double x = calculatePriceWithTax()*0.015;
-    return calculatePriceWithTax()-x;
+    double x=0.0;
+    if(Digital::getVariable()==true) {
+     x=_game->getPrice()*Digital::getDiscount();
+    }
+    else if(Digital::getVariable()==false){
+      x= _movie->getPrice()*Digital::getDiscount();
+    }
+    return x;
 }
 string Digital::toString(){
     stringstream s;
     if(Digital::getVariable()==true) {
-        s <<_g->toString();
-        s<<Digital::calculatePriceWithTax()<<std::endl;
+        s<<"Tipe Game: "<<"\n";
+        s <<_game->toString()<<"\n";
+        s<<"Price with discout: "<<Digital::calculateDiscount()<<"\n";
+        s<<"Price with tax: "<<Digital::calculatePriceWithTax()<<"\n";
     }
     else if(Digital::getVariable()==false){
-        s<<_m->toString();
-        s<<Digital::calculatePriceWithTax()<<std::endl;
+        s<<"Tipe Movie: "<<"\n";
+        s<<_movie->toString()<<"\n";
+        s<<"Price with discout: "<<Digital::calculateDiscount()<<"\n";
+        s<<"Price with tax: "<<Digital::calculatePriceWithTax()<<"\n";
     }
     return s.str();
 }
