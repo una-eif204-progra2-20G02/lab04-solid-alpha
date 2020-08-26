db54244e58d394e56ea17bf086b51d4456543626
steveMadrigal <68918625+steveMadrigal@users.noreply.github.com>
2020-08-26 12:09:11 -0600

    Final v1.00

--- Physical.h
+++ Physical.h
@@ -5,19 +5,16 @@
 #ifndef LAB04_SOLID_PHYSICAL_H
 #define LAB04_SOLID_PHYSICAL_H
 #include "Game.h"
-#include "Movie.h"
 #include "IEspecificationsDiscountClass.h"
 #include "ICalculusTax.h"
 
 class Physical : public ICalculusTax,public IEspecificationsDiscount{
 public:
     Physical();
-    Physical(double,double,std::string, Game*,Movie*);
+    Physical(double,double,std::string, Game*);
     ~Physical();
     virtual Game* getGame()const;
     virtual void setGame(Game*);
-    virtual Movie* getMovie()const;
-    virtual void setMovie(Movie*);
     virtual std::string getProductDimensions()const;
     virtual double getItemWeight()const;
     virtual double getTax()const;
@@ -25,12 +22,10 @@ public:
     virtual void setItemWeight(double);
     virtual void setTax(double);
     virtual double calculatePriceWithTaxGame() const override;
-    virtual double calculatePriceWithTaxMovie() const override;
     virtual std::string toString()const;
 private:
     double _tax;
     Game* _game=new Game();
-    Movie* _movie=new Movie();
     double _itemWeight;
     std::string _productDimensions;
 };
